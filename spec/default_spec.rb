ad87081e47a1ccc2169ef7283320b6fe200ea26b
Jeff Byrnes <jeff@evertrue.com>
2014-05-09 11:54:31 -0400

    Add stubs for ChefSpec & Test Kitchen tests

---
+++ spec/default_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe 'motd::default' do
+  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+
+  # TODO: Add tests
+end
---
+++ test/integration/default/serverspec/default_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe 'MOTD' do
+  # TODO: Add tests
+end
