c219c7c5dde14469f8642fee44085936fa058609
Jeff Byrnes <jeff@evertrue.com>
2014-05-09 11:21:14 -0400

    Change name to et_motd, bump to v1.0.0

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'motd'
+name             'et_motd'
 maintainer       'Chris Aumann'
 maintainer_email 'me@chr4.org'
 license          'GNU Public License 3.0'
 description      'Installs/Configures motd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.1'
+version          '1.0.0'
 depends          'chef_handler'
